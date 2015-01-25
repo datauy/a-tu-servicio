ea44ed2074ff16126d3aff8a717100a5ae953a7e
Lucia Escanellas <raviolicode@gmail.com>
2015-01-25 19:04:06 -0200

    Update listing with new data

--- scripts/controllers/listing.js
+++ scripts/controllers/listing.js
@@ -2,9 +2,10 @@
 
 angular.module('saludEnCifrasApp')
   .controller('ListingController', function ($scope, $http) {
-    $http.get('data/listing.json')
+    $http.get('data/details.json')
       .then(function(res) {
         $scope.providers = res.data['providers'];
+        console.log($scope.providers[0].precios);
       });
   });
 
